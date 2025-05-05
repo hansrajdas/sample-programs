6e8d2ebbd4bcbb96872f6a4687f84ecaa1083adf
Hansraj Das <raj.das.136@gmail.com>
2025-05-05 21:37:03 +0530

    Format interfaces sample program file

--- golang/interface.go
+++ golang/interface.go
@@ -1,42 +1,50 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "reflect"
+	"encoding/json"
+	"fmt"
+	"reflect"
 )
 
 type other struct {
-    OtherValue interface{}
+	OtherValue interface{}
 }
 
 type s1 struct {
-    V1 string
+	V1 string
 }
 
 type s2 struct {
-    V2 string
+	V2 string
 }
 
 func myjson(data interface{}) {
-    var b []byte
-    if reflect.TypeOf(data).Kind() == reflect.String {
-        b, _ = json.Marshal(&other{OtherValue: data})
-    } else {
-        b, _ = json.Marshal(data)
-    }
-    fmt.Println(string(b))
+	var b []byte
+	if reflect.TypeOf(data).Kind() == reflect.String {
+		b, _ = json.Marshal(&other{OtherValue: data})
+	} else {
+		b, _ = json.Marshal(data)
+	}
+	fmt.Println(string(b))
 }
 
 func main() {
-    d1 := &s1{
-        V1: "value1",
-    }
-    myjson(d1)
+	d1 := &s1{
+		V1: "value1",
+	}
+	myjson(d1)
 
-    d2 := &s2{
-        V2: "value2",
-    }
-    myjson(d2)
-    myjson("hello")
+	d2 := &s2{
+		V2: "value2",
+	}
+	myjson(d2)
+	myjson("hello")
 }
+
+/*
+Output
+------
+{"V1":"value1"}
+{"V2":"value2"}
+{"OtherValue":"hello"}
+*/
